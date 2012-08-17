6edc25ae8cbd2f3f37837afccf70e6bcae18c9b6
Luan Almeida <luanlmd@gmail.com>
2012-08-17 18:01:07 -0300

    new awesome tests

--- tests/library/Whippet/WhippetTest.php
+++ tests/library/Whippet/WhippetTest.php
@@ -10,9 +10,13 @@ class WhippetTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue(is_numeric($w->getExecutionTime()));
     }
 
-    function test_request_localhost_root_properties()
+
+    /**
+     * @dataProvider providerForDispatcher
+     *
+     */
+    function test_request_localhost_root_properties($server, $ret)
     {
-        $server = include '_SERVER_localhost_WhippetPHP.php';
         $config =  array(
             'useEtags' => false,
             'frontController' => 'MasterController',
@@ -24,17 +28,114 @@ class WhippetTest extends \PHPUnit_Framework_TestCase
 
         //var_dump($dispatcher);
 
-        $this->assertEquals($dispatcher->request->controller, 'IndexController');
-        $this->assertEquals($dispatcher->request->action, 'index');
-        $this->assertEquals($dispatcher->request->virtualRoot, '/WhippetPHP/');
-        $this->assertEquals($dispatcher->request->path, 'index\index');
-        $this->assertEquals($dispatcher->request->url, '');
-        $this->assertCount(0, $dispatcher->request->pars);
+        $this->assertEquals($dispatcher->request->controller, $ret[0]);
+        $this->assertEquals($dispatcher->request->action, $ret[1]);
+        $this->assertEquals($dispatcher->request->virtualRoot, $ret[2]);
+        $this->assertEquals($dispatcher->request->path, $ret[3]);
+        $this->assertEquals($dispatcher->request->url, $ret[4]);
+        $this->assertCount($ret[5], $dispatcher->request->pars);
 
-        $this->assertEquals($dispatcher->request->response->layout, 'default');
+        $this->assertEquals($dispatcher->request->response->layout, $ret[6]);
 
-        $this->assertEquals($dispatcher->request->config->frontController, 'MasterController');
-        $this->assertEquals($dispatcher->request->config->useEtags, false);
-        $this->assertEquals($dispatcher->request->config->viewEngine, 'phtml');
+        $this->assertEquals($dispatcher->request->config->frontController, $ret[7]);
+        $this->assertEquals($dispatcher->request->config->useEtags, $ret[8]);
+        $this->assertEquals($dispatcher->request->config->viewEngine, $ret[9]);
+    }
+
+    function providerForDispatcher()
+    {
+        return array(
+            array(
+                array(
+                  'APPLICATION_ENV' => 'development',
+                  'DOCUMENT_ROOT' => '/var/www',
+                  'SCRIPT_FILENAME' => '/var/www/WhippetPHP/whippet/bootstrap.php',
+                  'REQUEST_METHOD' => 'GET',
+                  'QUERY_STRING' => '',
+                  'REQUEST_URI' => '/WhippetPHP/',
+                ),
+                array(
+                    'IndexController',
+                    'index',
+                    '/WhippetPHP/',
+                    'index\index',
+                    '',
+                    0,
+                    'default',
+                    'MasterController',
+                    false,
+                    'phtml'
+                )
+            ),
+
+            array(
+                array(
+                  'APPLICATION_ENV' => 'development',
+                  'DOCUMENT_ROOT' => '/var/www',
+                  'SCRIPT_FILENAME' => '/var/www/WhippetPHP/whippet/bootstrap.php',
+                  'REQUEST_METHOD' => 'GET',
+                  'QUERY_STRING' => '',
+                  'REQUEST_URI' => '/WhippetPHP/teste',
+                ),
+                array(
+                    'TesteController',
+                    'index',
+                    '/WhippetPHP/',
+                    'teste\index',
+                    'teste',
+                    0,
+                    'default',
+                    'MasterController',
+                    false,
+                    'phtml'
+                )
+            ),
+
+            array(
+                array(
+                  'APPLICATION_ENV' => 'development',
+                  'DOCUMENT_ROOT' => '/var/www/WhippetPHP',
+                  'SCRIPT_FILENAME' => '/var/www/WhippetPHP/whippet/bootstrap.php',
+                  'REQUEST_METHOD' => 'GET',
+                  'QUERY_STRING' => '',
+                  'REQUEST_URI' => '/',
+                ),
+                array(
+                    'IndexController',
+                    'index',
+                    '/',
+                    'index\index',
+                    '',
+                    0,
+                    'default',
+                    'MasterController',
+                    false,
+                    'phtml'
+                )
+            ),
+
+            array(
+                array(
+                  'APPLICATION_ENV' => 'development',
+                  'DOCUMENT_ROOT' => '/var/www/WhippetPHP',
+                  'SCRIPT_FILENAME' => '/var/www/WhippetPHP/whippet/bootstrap.php',
+                  'REQUEST_METHOD' => 'GET',
+                  'QUERY_STRING' => '',
+                  'REQUEST_URI' => '/teste',
+                ),
+                array(
+                    'TesteController',
+                    'index',
+                    '/',
+                    'teste\index',
+                    'teste',
+                    0,
+                    'default',
+                    'MasterController',
+                    false,
+                    'phtml'
+                )
+            ),
+        );
     }
 }
