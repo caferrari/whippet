f24cf16787e322507c3f8e3f852900133cf7074b
Luan Almeida <luanlmd@gmail.com>
2012-08-17 10:35:43 -0300

    Welcome page added

---
+++ app/Controller/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Application\Controller;
+
+class IndexController extends \Whippet\Controller
+{
+
+    public function index()
+    {
+        $this->controllerFile = __FILE__;
+    }
+
+}
